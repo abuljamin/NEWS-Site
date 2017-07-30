f5e577d2609e1d65064b8a3b58f9e4205e81ce96
Jamin Abul <Jamin Abul>
2017-07-30 17:31:34 +0700

    Signed-off-by: Jamin Abul <Jamin Abul>

---
+++ blocks/menu.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abulj
+ * Date: 7/17/2017
+ * Time: 9:18 PM
+ */
+echo "Hello PHP1111111111111111";
---
+++ pages/process.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abulj
+ * Date: 7/30/2017
+ * Time: 2:56 PM
+ */
+
+if (isset($_POST["login"])) {
+    echo $_POST["login"] . "<br>";
+} else {
+    echo "fail";
+}
